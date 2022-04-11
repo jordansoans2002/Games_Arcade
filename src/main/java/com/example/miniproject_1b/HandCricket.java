103db98c5f17268c28fa36c657bf9966865fa510
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2022-04-11 13:59:43 +0530

    Update HandCricket.java
    batting and bowling, batting and bowling methods can be merged into one.

--- src/main/java/com/example/miniproject_1b/HandCricket.java
+++ src/main/java/com/example/miniproject_1b/HandCricket.java
@@ -3,33 +3,96 @@ package com.example.miniproject_1b;
 import java.util.Scanner;
 
 public class HandCricket {
-    static int totalRuns=0,ballsBowled=0;
+    static int totalRuns=0,ballsBowled=0,innings=1,target;
     public static void main(String[] args) {
+        handCricket_client.main();
         System.out.println("LETS START THE GAME");
         System.out.println("HAND CRICKET");
+        int toss=handCricket_client.receive();
+        if(toss==1)
+            batting();
+        else
+            bowling();
+    }
+    public static void batting(){
+        totalRuns=0;
+        ballsBowled=0;
         Scanner sc=new Scanner(System.in);
         while(true) {
-            System.out.println("ENTER RUNS");
-            int runs = sc.nextInt();
-            if(runs<0 || runs>6)
+            System.out.println("ENTER RUN");
+            int run = sc.nextInt();
+            if(run<0 || run>6)
             {
                 System.out.println("INVALID INPUT");
                 continue;
             }
-            int ball = (int) (Math.random() * 7);
+            handCricket_client.send(run);
+
+            int ball = handCricket_client.receive();
             System.out.println(ball);
-            if (runs == ball) {
+            if (run == ball) {
                 System.out.println("OUT");
+                if(innings==1){
+                    target=totalRuns;
+                    System.out.println("defend: "+target);
+                    innings=2;
+                    bowling();
+                }
+                else
+                    System.out.println("you lost");
                 break;
             }
-            totalRuns = totalRuns + runs;
+            totalRuns = totalRuns + run;
             ballsBowled++;
+            if(innings==2&&totalRuns>=target){
+                System.out.println("you won");
+                break;
+            }
             if(ballsBowled%6==0)
                 System.out.println("OVERS COMPLETED "+ballsBowled/6 );
             System.out.println("TOTAL RUNS =" + totalRuns);
             System.out.println("TOTAL BALLS =" + ballsBowled);
         }
 
+    }
+
+    public static void bowling(){
+        totalRuns=0;
+        ballsBowled=0;
+        Scanner sc=new Scanner(System.in);
+        while(true) {
+            System.out.println("ENTER BALL");
+            int ball = sc.nextInt();
+            if(ball<0 || ball>6)
+            {
+                System.out.println("INVALID INPUT");
+                continue;
+            }
+            handCricket_client.send(ball);
+
+            int runs = handCricket_client.receive();
+            System.out.println(runs);
+            if (runs == ball) {
+                System.out.println("OUT");
+                if(innings==1){
+                    target=totalRuns;
+                    System.out.println("target: "+target);
+                    innings=2;
+                    batting();
+                }
+                break;
+            }
+            totalRuns = totalRuns + runs;
+            ballsBowled++;
+            if(innings==2&&totalRuns>target){
+                System.out.println("you lost");
+                break;
+            }
+            if(ballsBowled%6==0)
+                System.out.println("OVERS COMPLETED "+ballsBowled/6 );
+            System.out.println("TOTAL RUNS =" + totalRuns);
+            System.out.println("TOTAL BALLS =" + ballsBowled);
+        }
     }
 
 }
